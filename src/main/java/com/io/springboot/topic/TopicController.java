403373925ec946e907cee962c9e143aadcc05e64
anitha <s.anithapriyadharshini@gmail.com>
2019-06-12 22:45:44 +1000

    lombok

--- src/main/java/com/io/springboot/topic/TopicController.java
+++ src/main/java/com/io/springboot/topic/TopicController.java
@@ -1,5 +1,6 @@
 package com.io.springboot.topic;
 
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,8 +12,9 @@ public class TopicController {
 
     @RequestMapping("/topics")
     public List<Topic> returnTopic(){
-        return Arrays.asList( new Topic("java","java","java descp"),
-                new Topic("sql","sql","sql descp"),
-                new Topic("api","api","api descp"));
+        return Arrays.asList( Topic.builder().id("1").name("java").description("java descp").build(),
+                Topic.builder().id("2").name("sql").description("sql descp").build(),
+                Topic.builder().id("3").name("api").description("api descp").build());
+
     }
 }
